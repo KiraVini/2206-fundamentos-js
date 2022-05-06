cf38a619e3f769a4c21af8f22e56ca00e510d5c0
KiraVini <viniciusakira@hotmail.com>
2022-05-06 16:21:01 -0300

    Exemplos da Aula1

---
+++ type-string.js
@@ -0,0 +1,26 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = 'O Leo disse "oi!"';
+console.log(citacao);
+
+//Concatenação (+)
+const citacao2 = "Meu nome é ";
+const meuNome = "Leonardo";
+
+console.log(citacao2 + meuNome);
+
+//Template string OU template literal
+
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
