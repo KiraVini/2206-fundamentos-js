379674ce69559e3dde547bc58d22f4a1e49a4da1
KiraVini <viniciusakira@hotmail.com>
2022-05-13 16:43:58 -0300

    Arrays - Filter and Reduce

---
+++ juntando-turmas.js
@@ -0,0 +1,6 @@
+const salaDePython = ['Melissa', 'Helena', 'Rodrigo'];
+const salaDeJacascript = ['Ju', 'Leo', 'Raquel'];
+
+const salasUnificadas = salaDePython.concat(salaDeJacascript);
+
+console.log(salasUnificadas)
---
+++ media.js
@@ -0,0 +1,9 @@
+const alunos = ['João', 'Juliana', 'Caio', "Ana"];
+
+const mediaDosalunos = [10,7,9,6];
+
+let listaDeNotasEAlunos = [alunos, mediaDosalunos];
+
+console.log(`${listaDeNotasEAlunos[0][0]}`);
+
+console.log(`${listaDeNotasEAlunos[0][0]}, sua média é ${listaDeNotasEAlunos[1][0]}`);
