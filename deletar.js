c7c79fd45988a040a5bcf7d38090d0ec2c7387e5
KiraVini <viniciusakira@hotmail.com>
2022-05-17 18:08:03 -0300

    Fundamentos parte 2

---
+++ deletar.js
@@ -0,0 +1,20 @@
+const objPersonagem = {
+    nome: "Gandalf",
+    classe: "mago",
+    nivel: "20",
+    aliado: {
+      nome: "Saruman",
+      classe: "mago"
+    },
+    status: "desaparecido"
+   }
+
+   const delProp = delete objPersonagem.aliado
+   const delPropInexistente = delete objPersonagem["endereco"]
+   
+   console.log(delProp) //true
+   console.log(delPropInexistente) //true
+   
+   console.log(objPersonagem.aliado) //undefined
+   console.log(objPersonagem.status) //undefined
+   console.log(objPersonagem) //undefined
---
+++ lista-unica.js
@@ -0,0 +1,29 @@
+const clientes = [
+    {
+    nome: "André",
+    cpf : "12543652266",    
+    dependentes :[{
+            nome:"Sara",
+            parentesco:"filha",
+            dataNasc:"20/03/2011"
+        },
+        {
+            nome:"Samia Maria",
+            parentesco:"filha",
+            dataNasc:"04/01/2014"
+        }]
+    },
+    {
+    nome: "Juliana",
+    cpf : "56767867867",    
+        dependentes :[{
+            nome:"Sophia",
+            parentesco:"filha",
+            dataNasc:"30/08/2020"
+        }]
+    }
+]
+
+const listaDependentes = [...clientes[0].dependentes, ...clientes[1].dependentes]
+
+console.table(listaDependentes)
